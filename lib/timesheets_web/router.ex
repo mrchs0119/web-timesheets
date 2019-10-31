343624f9ee34708a60c31a5ff143e97c4a6b246e
chihao sun <mr.chs0119@gmail.com>
2019-10-31 11:26:24 -0400

    test

--- lib/timesheets_web/router.ex
+++ lib/timesheets_web/router.ex
@@ -4,6 +4,7 @@ defmodule TimesheetsWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
+    plug TimesheetsWeb.Plugs.FetchCurrentUser
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
@@ -17,6 +18,8 @@ defmodule TimesheetsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController
+    resources "/sessions", SessionController, only: [:create, :delete], singleton: true
   end
 
   # Other scopes may use custom stacks.
